973de1fbdb5c52825eeb7e3945de4c4cd5501fd9
Vitaliy Rodyna <ukrdao@gmail.com>
2022-08-24 15:37:46 +0300

    added variables

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ var fpsAverage;
 
 var canvas, notes, context;
 var width, height;
+
+var centerPlanet;
+var mercury, venus, earth, moon, jupiter, io, europa, ganymede, callisto, sun;
+
+var drawPath = false;
+
+var speedRation = 1.0;
+
+var zoom = 1.0;
